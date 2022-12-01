d705fec72c59e3440daf92a620998a346fab5ad3
Ole <olakas99@wp.pl>
2022-12-01 01:34:32 +0100

    robot init

--- scripts/simulation.js
+++ scripts/simulation.js
@@ -13,11 +13,25 @@ function run() {
 function init_simulation() {
     const simulation = document.getElementById("simulation_svg");
 
-    const w = simulation.style.width;
-    const h = simulation.style.height;
+    // const w = simulation.getBBox().width;
+    // const h = simulation.getBBox().height;
 
-    console.log(w);
-    console.log(h);
+    // console.log(w);
+    // console.log(h);
+
+    
+    simulation.setAttribute('height', '512');
+    simulation.setAttribute('width', '512');
+
+
+    const robot = simulation.getElementById("robot");
+    console.log(robot)
+
+
+    // MOVE TO POINT
+    robot.setAttribute("x", "50");
+    robot.setAttribute("y", "50");
+    
 }
 
 export { init_simulation }
